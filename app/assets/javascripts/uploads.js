d2858e01e79f77536b61eda991e8b43fd087ea21
Joseph-N <josendungu@hotmail.com>
2014-07-20 22:04:20 +0300

    added delete file from server functionality

--- app/assets/javascripts/uploads.js
+++ app/assets/javascripts/uploads.js
@@ -10,6 +10,28 @@ $(document).ready(function(){
 		// our rails app
 		paramName: "upload[image]",
 		// show remove links on each image upload
-		addRemoveLinks: true
+		addRemoveLinks: true,
+		// if the upload was successful
+		success: function(file, response){
+			// find the remove button link of the uploaded file and give it an id
+			// based of the fileID response from the server
+			$(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
+			// add the dz-success class (the green tick sign)
+			$(file.previewElement).addClass("dz-success");
+		},
+		//when the remove button is clicked
+		removedfile: function(file){
+			// grap the id of the uploaded file we set earlier
+			var id = $(file.previewTemplate).find('.dz-remove').attr('id'); 
+
+			// make a DELETE ajax request to delete the file
+			$.ajax({
+				type: 'DELETE',
+				url: '/uploads/' + id,
+				success: function(data){
+					console.log(data.message);
+				}
+			});
+		}
 	});	
 });
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -7,12 +7,21 @@ class UploadsController < ApplicationController
     @upload = Upload.create(upload_params)
     if @upload.save
       # send success header
-      render json: { message: "success" }, :status => 200
+      render json: { message: "success", fileID: @upload.id }, :status => 200
     else
       #  you need to send an error header, otherwise Dropzone
       #  will not interpret the response as an error:
       render json: { error: @upload.errors.full_messages.join(',')}, :status => 400
     end     
+  end
+
+  def destroy
+    @upload = Upload.find(params[:id])
+    if @upload.destroy    
+      render json: { message: "File deleted from server" }
+    else
+      render json: { message: @upload.errors.full_messages.join(',') }
+    end
   end
 
   private
